9587958bbd00652a96e82934e28a50c5c52c4829
Apu Chakraborty <webapu96@gmail.com>
2021-02-02 22:57:22 +0530

    initial commit

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Home.css'
+
+const Home = () => {
+    return (
+        <>
+          <img src="https://images-eu.ssl-images-amazon.com/images/G/31/img20/Wireless/SamsungM/M02/Launch/2ndFeb/Rv4/D19805912_IN_WLME_SamsungGalaxy_M02s_New_Launch_DesktopTallHero_1500x600._CB661306685_.jpg"
+          className="home__image"
+          alt=""
+          />  
+        </>
+    )
+}
+
+export default Home
