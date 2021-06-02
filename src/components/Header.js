5e572d623402b8051c06c0c5addc816ef69984b9
apu-chakraborty <apu.chakraborty@marqueetechnology.com>
2021-06-02 22:06:27 +0530

    check ssh

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,59 +1,47 @@
-import React from 'react'
-import './Header.css'
-import SearchIcon from '@material-ui/icons/Search';
-import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 
 const Header = () => {
-    return (
-        <div className="header">
-            <div className="logo">
-            <img 
-            src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
-             className="header__logo" alt=""/>
+  return (
+    <div className="header">
+      <div className="logo">
+        <img
+          src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+          className="header__logo"
+          alt=""
+        />
+      </div>
 
-            </div>
+      <div className="header__search">
+        <input type="text" className="header__searchInput" />
+        <SearchIcon className="header__searchIcon" />
+      </div>
 
-            <div className="header__search">
-<input type="text" className="header__searchInput" />
-<SearchIcon className="header__searchIcon" />
-            </div>
+      <div className="header__nav">
+        <div className="header__options">
+          <span className="header__optionLineone">Hello Guest 🎧</span>
+          <span className="header__optionLinetwo">Sign In</span>
+        </div>
 
-            <div className="header__nav">
-            <div className="header__options">
-            <span className="header__optionLineone">
-Hello Guest
-</span>
-<span className="header__optionLinetwo">
-Sign In
-</span>
-            </div>
+        <div className="header__options">
+          <span className="header__optionLineone">hello</span>
+          <span className="header__optionLinetwo">hello</span>
+        </div>
 
-            <div className="header__options">
-            <span className="header__optionLineone">
-hello
-</span>
-<span className="header__optionLinetwo">
-hello
-</span>
-            </div>
-
-            <div className="header__options">
-            <span className="header__optionLineone">
-hello
-</span>
-<span className="header__optionLinetwo">
-hello
-</span>
-            </div>
+        <div className="header__options">
+          <span className="header__optionLineone">hello</span>
+          <span className="header__optionLinetwo">hello</span>
+        </div>
 
         <div className="header__optionBasket">
-<ShoppingBasketIcon />
-<span className="header__optionLinetwo header__basketCount">0</span>
+          <ShoppingBasketIcon />
+          <span className="header__optionLinetwo header__basketCount">0</span>
         </div>
-            </div>
-            
-        </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
